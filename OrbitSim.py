8282193779383e05ac3f189bdfef5efc66992b77
Danny <ddeneau@terpmail.umd.edu>
2020-02-13 23:29:32 -0500

    Added parameters to constructor for Mechanics class

--- OrbitSim.py
+++ OrbitSim.py
@@ -3,6 +3,7 @@
 import numpy
 import scipy.constants
 import pygame
+import random
 WIDTH = 1200
 HEIGHT = 800
 
@@ -24,34 +25,112 @@ def adjust_coordinates(body):
 
 class Mechanics:
 
-    def __init__(self, semi_major, semi_minor, focus, eccentricity):
+    def __init__(self, semi_major, semi_minor, focus):
         self.semi_major = semi_major   # The semi-major axis
         self.semi_minor = semi_minor  # The semi-minor axis
         self.focus = focus  # Focus of ellipse
-        self.eccentricity = eccentricity
-        self.theta = 0.01  # Angle between radius and x-axis
-        self.phi = 0.01  # Angle between radius, from focus, and x-axis
-        self.star = Body(100, 45, int(WIDTH / 2), int(HEIGHT / 2))  # main star (mass, radius)
-        self.planet = Body(1, 2, int(WIDTH / 2) + self.semi_major, int(HEIGHT / 2))    # First planet (mass, radius)
+        self.star = Body(50, 25, int(WIDTH / 2), int(HEIGHT / 2), 0, 0, 0)  # main star (mass, radius)
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))  # Making of the screen
+        self.bodies = list()
+        self.generate_bodies()
+
+    def generate_bodies(self):
+        for i in range(3):
+            mass = random.randint(1, 5)
+            radius = mass
+            x = int(WIDTH / random.randint(1, 5))
+            y = int(HEIGHT / random.randint(1, 5))
+            e = random.randint(1, 1000) / 1000
+            m = i / 100
+            self.bodies.append(Body(mass, radius, x, y, e, e + m, e + m))
 
     # Updates alpha with new angular acceleration.
-    def compute_force(self):
+    def compute_force(self, body):
         mass = self.star.mass
-        r_squared = numpy.power(self.planet.distance, 2)
+        r_squared = numpy.power(body.distance, 2)
 
-        self.planet.alpha = scipy.constants.gravitational_constant * mass / r_squared
+        body.alpha = scipy.constants.gravitational_constant * mass / r_squared
 
     # Calculates new angular velocity
-    def compute_angular_velocity(self):
-        self.planet.omega = numpy.sqrt(self.planet.alpha / (self.planet.mass * numpy.power(self.semi_major, 3)))
+    def compute_angular_velocity(self, body):
+        body.omega = numpy.sqrt(body.alpha / (body.mass * numpy.power(self.semi_major, 3)))
 
     # Update x and y values of planet based off of Kepler's Law's
-    def compute_radial_vector(self):
-        x = (self.semi_major * numpy.cos(self.theta)) - self.eccentricity
-        y = (self.semi_major * numpy.sin(self.theta) * (1 - numpy.power(self.eccentricity, 2)))
+    def compute_radial_vector(self, body):
+        x = (self.semi_major * numpy.cos(body.theta)) - body.eccentricity
+        y = (self.semi_major * numpy.sin(body.theta) * (1 - numpy.power(body.eccentricity, 2)))
 
-        self.planet.update_state(x, y, self.theta, self.planet.omega)
+        body.update_state(x, y, body.theta, body.omega)
+
+    # Computations of tick of the simulation.
+    def run_sim_frame(self):
+        for body in self.bodies:
+            self.screen.fill((100, 100, 100))
+            body.check_angle()  # Make sure no angle exceeds 2pi rads
+            self.compute_force(body)  # Force and angular acceleration equation
+            self.compute_angular_velocity(body)  # Angular velocity equation.
+            self.compute_radial_vector(body)  # Updates x and y positions.
+            body.update_angle()  # Increments angle to move time forward.
+
+    # Coordinate adjustment to fit with pygame coordinate scheme.
+
+    # Initializes simulation as well as pygame window.
+    def init_graphics(self):
+        clock = pygame.time.Clock()
+        pygame.display.set_caption('Orbit Simulation')  # Name for the window
+        self.screen.fill((100, 100, 100))  # This syntax fills the background colour
+        pygame.display.flip()
+
+        r_planets = list()
+        r_star = self.star.radius
+
+        for body in self.bodies:
+            r_planets.append(body.radius)
+
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+            self.run_sim_frame()
+
+            for body in self.bodies:
+                adjust_coordinates(body)
+
+            pygame.draw.circle(self.screen, (200, 0, 0), (self.star.x, self.star.y), r_star, r_star)
+
+            for body in self.bodies:
+                pygame.draw.circle(self.screen, (0, 200, 0), (body.x, body.y), body.radius, body.radius)
+
+            pygame.display.update()
+            clock.tick(100)
+
+        pygame.quit()
+
+
+# A object that experience state changes in relation to physical forces.
+class Body:
+
+    def __init__(self, mass, radius, x, y, eccentricity, theta, phi):
+        self.mass = mass  # Mass of body.
+        self.radius = radius  # Radius of body
+        self.distance = numpy.sqrt(x**2 + y**2) # Starting distance from star on x-axis, Starting at 0 radians.
+        self.x = x  # x coordinate
+        self.y = y  # y coordinate
+        self.omega = 0  # Angular velocity
+        self.alpha = 0  # Angular acceleration
+        self.theta = theta  # Angle between radius and x-axis
+        self.phi = phi  # Angle between radius, from focus, and x-axis
+        self.eccentricity = eccentricity
+
+    # Change state of body based on parameters.
+    def update_state(self, x, y, alpha, omega):
+        self.x = x
+        self.y = y
+        self.alpha = alpha
+        self.omega = omega
+        print(x, y, self.eccentricity)
 
     # Keeps us between 0 and 2Pi
     def check_angle(self):
@@ -63,72 +142,9 @@ class Mechanics:
     # Updates value of angle between semi-major axis and the vector from the star to the planet.
     def update_angle(self):
         self.theta += numpy.arctan(numpy.tan(self.phi / 2) /
-                                   numpy.sqrt((1 + self.eccentricity)/(1 - self.eccentricity)))
-
-    # Computations of tick of the simulation.
-    def run_sim_frame(self):
-        self.screen.fill((100, 100, 100))
-        self.check_angle()  # Make sure no angle exceeds 2pi rads
-        self.compute_force()  # Force and angular acceleration equation
-        self.compute_angular_velocity()  # Angular velocity equation.
-        self.compute_radial_vector()  # Updates x and y positions.
-        self.update_angle()  # Increments angle to move time forward.
-
-    # Coordinate adjustment to fit with pygame coordinate scheme.
-
-    # Initializes simulation as well as pygame window.
-    def init_graphics(self):
-        clock = pygame.time.Clock()
-        pygame.display.set_caption('Orbit Simulation')  # Name for the window
-        self.screen.fill((100, 100, 100))  # This syntax fills the background colour
-        pygame.display.flip()
-        r_star = self.star.radius
-        r_planet = self.planet.radius
-        running = True
-        while running:
-            for event in pygame.event.get():
-                if event.type is pygame.KEYDOWN:
-                    if event.key is pygame.K_d:
-                        self.theta += 0.2
-                        self.phi += 0.2
-                    elif event.key is pygame.K_a:
-                        self.theta -= 0.2
-                        self.theta -= 0.2
-
-                if event.type == pygame.QUIT:
-                    running = False
-
-            self.run_sim_frame()
-            adjust_coordinates(self.planet)
-            pygame.draw.circle(self.screen, (200, 0, 0), (self.star.x, self.star.y), r_star, r_star)
-            pygame.draw.circle(self.screen, (0, 200, 0), (self.planet.x, self.planet.y), r_planet, r_planet)
-            pygame.display.update()
-            clock.tick(100)
-
-        pygame.quit()
-
-
-# A object that experience state changes in relation to physical forces.
-class Body:
-
-    def __init__(self, mass, radius, x, y):
-        self.mass = mass  # Mass of body.
-        self.radius = radius  # Radius of body
-        self.distance = 500  # Starting distance from star on x-axis, Starting at 0 radians.
-        self.x = x  # x coordinate
-        self.y = y  # y coordinate
-        self.omega = 0  # Angular velocity
-        self.alpha = 0  # Angular acceleration
-
-    # Change state of body based on parameters.
-    def update_state(self, x, y, alpha, omega):
-        self.x = x
-        self.y = y
-        self.alpha = alpha
-        self.omega = omega
-        print(x, y)
+                                   numpy.sqrt((1 + self.eccentricity) / (1 - self.eccentricity)))
 
 
 # Runs program.
-m = Mechanics(400, 200, 175, 0.5)
+m = Mechanics(200, 150, 100)
 m.init_graphics()
